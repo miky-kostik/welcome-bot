e5553b97c6e2c5f16ebdfcd302c20e9cd4242d69
miky_kostik <svehlamikulas01@gmail.com>
2026-08-19 11:42:37 +0200

    Add image welcome message with canvas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
+const { createCanvas, loadImage } = require('canvas');
 
 const client = new Client({
     intents: [
@@ -12,20 +13,40 @@ client.once('clientReady', () => {
     console.log(`Bot je přihlášený jako ${client.user.tag}`);
 });
 
-client.on('guildMemberAdd', (member) => {
+client.on('guildMemberAdd', async (member) => {
     const channel = member.guild.channels.cache.find(
         (ch) => ch.name === '🚪the-beggining'
     );
 
     if (!channel) return;
 
-    channel.send(`Vítej na serveru, ${member}! 👋`);
+    const imageBuffer = await createWelcomeImage(member.user.username);
+
+    channel.send({
+        files: [{ attachment: imageBuffer, name: 'welcome.png'}],
+    });
 });
+
+async function createWelcomeImage(username) {
+    const canvas = createCanvas(1000, 500);
+    const ctx = canvas.getContext('2d');
+
+    const background = await loadImage('./assets/welcome-background.webp');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.font = 'bold 60px Arial';
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.fillText(`Vítej, ${username}!`, canvas.width / 2, canvas.height / 2);
+
+    return canvas.toBuffer();
+};
 
 client.login(process.env.DISCORD_TOKEN);
 
 const express = require('express');
 const app = express();
+app.disable('x-powered-by');
 app.get('/', (req, res) => res.send('Bot běží!'));
 app.listen(process.env.PORT || 3000, () => {
     console.log('Webserver pro Railway spuštěn.');
