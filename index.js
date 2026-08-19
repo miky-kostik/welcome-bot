a50cd9cde2b6a541d4116f8e7b1e6423fe67af99
miky_kostik <svehlamikulas01@gmail.com>
2026-08-19 13:16:16 +0200

    Convert background image to JPG

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.once('clientReady', () => {
 
 client.on('guildMemberAdd', async (member) => {
     const channel = member.guild.channels.cache.find(
-        (ch) => ch.name === '🚪the-beggining'
+        (ch) => ch.name === '👋-welcome'
     );
 
     if (!channel) return;
